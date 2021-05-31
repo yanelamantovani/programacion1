15875f983577c8c8602293b309aed8c6d8f1eae5
yanelamantovani <yanelamantovani@icloud.com>
2021-05-31 17:55:54 -0300

    Hasta Ejercicio 12 Clase 8

--- src/Clase8/Tp8_Ej12.java
+++ src/Clase8/Tp8_Ej12.java
@@ -1,26 +1,25 @@
 /*
-Carga de matrices con secuencias de enteros y caracteres.
+Hacer un programa que dada la matriz de secuencias de caracteres definida y precargada, permita encontrar por cada fila
+la posición de inicio y fin de la anteúltima secuencia (considerar comenzar a buscarla a partir de la ultima posición de la fila).
 */
 
 package Clase8;
 
 import java.util.Random;
 
-public class Clase8_Ejemplo8 {
+public class Tp8_Ej12 {
     public static final int MAXFILAS = 4;
     public static final int MAXCOLUMNAS = 20;
     public static final int MAXVALOR = 9;
     public static final int MINVALOR = 1;
-    public static final double probabilityLetter = 0.4;
     public static final double probabilityNumber = 0.4;
 
     public static void main(String[] args) {
         int [][] intMatriz = new int[MAXFILAS][MAXCOLUMNAS];
-        char [][] charMatriz = new char[MAXFILAS][MAXCOLUMNAS];
+
         cargarMatrizSecuenciasEnteros(intMatriz);
         imprimirMatrizEnteros(intMatriz);
-        cargarMatrizSecuenciasCaracter(charMatriz);
-        imprimirMatrizCaracteres(charMatriz);
+        buscarAnteultimaSecuenciaMatriz(intMatriz);
     }
 
     public static void cargarMatrizSecuenciasEnteros(int [][] intMatriz) {
@@ -53,32 +52,32 @@ public class Clase8_Ejemplo8 {
         }
     }
 
-    public static void cargarMatrizSecuenciasCaracter(char [][] charMatriz) {
-        for (int fila = 0; fila < MAXFILAS; fila++) {
-            cargarArregloSecuenciasCaracter(charMatriz[fila]);
+    public static int buscarPosicionInicioSecuencia(int[][] intMatriz, int fila, int posicionFin) {
+        while (intMatriz[fila][posicionFin] != 0) {
+            posicionFin--;
         }
-        System.out.println("");
+        int posicionInicio = posicionFin + 1;
+        return posicionInicio;
     }
 
-    public static void cargarArregloSecuenciasCaracter(char [] array) {
-        Random r = new Random();
-        array[0] = ' ';
-        array[MAXCOLUMNAS - 1] = ' ';
-        for (int i = 1; i < MAXCOLUMNAS - 1; i++) {
-            if (r.nextDouble() > probabilityLetter) {
-                array[i] = (char) (r.nextInt(26) + 'a');
-            } else {
-                array[i] = ' ';
-            }
+    public static int buscarPosicionFinSecuencia(int[][] intMatriz, int fila) {
+        int posicion = MAXCOLUMNAS - 1;
+        while (intMatriz[fila][posicion] == 0) {
+            posicion--;
         }
+        return posicion;
     }
 
-    public static void imprimirMatrizCaracteres(char [][] charMatriz) {
+    public static void buscarAnteultimaSecuenciaMatriz(int[][] intMatriz) {
         for (int fila = 0; fila < MAXFILAS; fila++) {
-            for (int columna = 0; columna < MAXCOLUMNAS; columna++) {
-                System.out.print("[" + charMatriz[fila][columna] + "]");
+            int posicionFin = buscarPosicionFinSecuencia(intMatriz, fila);
+            int posicionInicio = buscarPosicionInicioSecuencia(intMatriz, fila, posicionFin);
+            int ultimaPosicionAnteultimaSecuencia = posicionInicio - 1;
+            while (intMatriz[fila][ultimaPosicionAnteultimaSecuencia] == 0) {
+                ultimaPosicionAnteultimaSecuencia--;
             }
-            System.out.println("");
+            int primerPosicionAnteultimaSecuencia = buscarPosicionInicioSecuencia(intMatriz, fila, ultimaPosicionAnteultimaSecuencia);
+            System.out.println("La anteúltima secuencia de la fila " + fila + " empieza en la posición " + primerPosicionAnteultimaSecuencia + " y termina en la posición " + ultimaPosicionAnteultimaSecuencia);
         }
     }
 }
